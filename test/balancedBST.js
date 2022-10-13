4b9aa4da90ee1449a17d56397a6a57539d23197b
Tyler Niemann <tntman98@gmail.com>
2022-10-12 17:53:37 -0700

    complete buildTree Tests & function

--- test/balancedBST.js
+++ test/balancedBST.js
@@ -2,23 +2,27 @@ import { mergeSort, removeDuplicates} from "./mergeSort"
 
 const newNode = (value) =>{
     return {
-     leftNode: null,
-     value : value || null,
-     rightNode : null
+     leftPart: null,
+     data : value,
+     rightPart : null
     }
  }
 
 const Tree = (arr) =>{
-    const array = [...removeDuplicates(mergeSort(arr))]
+  const array = [...removeDuplicates(mergeSort(arr))]
    
+  const buildTree = (arr,start,end) =>{
+    if(start > end) return null;
+    let mid = parseInt((start + end) / 2);
+    let node = newNode(arr[mid]);
+    node.leftPart = buildTree(arr, start, mid - 1);
+    node.rightPart = buildTree(arr, mid + 1, end);
+    return node;
+  };
 
-    const buildTree = (arr) =>{
+  let root =  buildTree(array, 0, array.length-1)
 
-    };
-
-    let root =  buildTree(array, 0, arr.length-1)
-
-    return array
+  return root
 }
 
 export { Tree }
--- test/balancedBST.test.js
+++ test/balancedBST.test.js
@@ -1,9 +1,64 @@
 import { Tree } from "./balancedBST";
 
-const arr = Tree([2,4,3,3])
+const arr = Tree([2, 5, 6, 235, 5, 236, 23, 62, 52, 345, 21, 35, 45, 346,])
 
 describe('Tree properties', () => {
-  test('Tree sorts unsorted array', () => {
-    expect(arr).toStrictEqual([2,3,4])
+  test("The tree should be built", () => {
+    expect(arr).toMatchObject({
+      data: 45,
+      leftPart: {
+        data: 6,
+        leftPart: {
+          data: 2,
+          leftPart: null,
+          rightPart: {
+            data: 5,
+            leftPart: null,
+            rightPart: null,
+          },
+        },
+        rightPart: {
+          data: 23,
+          leftPart: {
+            data: 21,
+            leftPart: null,
+            rightPart: null,
+          },
+          rightPart: {
+            data: 35,
+            leftPart: null,
+            rightPart: null,
+          },
+        },
+      },
+      rightPart: {
+        data: 235,
+        leftPart: {
+          data: 52,
+          leftPart: null,
+          rightPart: {
+            data: 62,
+            leftPart: null,
+            rightPart: null,
+          },
+        },
+        rightPart: {
+          data: 345,
+          leftPart: {
+            data: 236,
+            leftPart: null,
+            rightPart: null,
+          },
+          rightPart: {
+            data: 346,
+            leftPart: null,
+            rightPart: null,
+          },
+        },
+      },
+    });
+  });
+  test('root of Tree is 45', () => {
+    expect(arr).toMatchObject({data: 45})
   });
 });
